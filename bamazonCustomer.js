67408d5f086c0fd119d7f39615fc937572c5bc01
test <test@tests-MacBook-Pro.local>
2018-12-15 22:33:08 -0600

    buggin out

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,10 @@
+//================set up required variables==========================
+
 var mysql = require("mysql");
 var inquirer = require ("inquirer");
+//var table = require ("cli-table");
+
+//================Connect to MySQL Database==========================
 
 var connection = mysql.createConnection ({
     host: "localhost",
@@ -18,21 +23,73 @@ connection.connect(function (err) {
     
 });
 function showAll() {
-    console.log("Printing all from table products: "+ "\n\n")
-    var query = 'SELECT * FROM products';
-    connection.query(query, function(err, res) {
+    console.log("Printing id, product name and price from Bamazon: "+ "\n\n")
+   
+    connection.query("SELECT id,product_name,price FROM products", function(err, res) {
+       if (err) throw err;
         console.log(res);
         shopperQuestion();
     })
 };
-function shopperQuestion() {
-    inquirer.prompt ({
-        name: "action",
-        type: "input",
-        message: "What's the ID of the product you want to buy?"
 
-    }).then (function(answers) {
-        console.log(answers);
-    })
+//======================Inquirer prompt for customers========================
+function shopperQuestion() {
+    inquirer.prompt ([
+        {
+            name: "purchaseId",
+            type: "input",
+            message: "What is the ID number of the product you want to buy?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "What is the quantity you want to purchase?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ])
+
+    //=================This will display the order===============================
+
+    .then(function(answer) {
+        connection.query("SELECT * FROM products WHERE id=?", answer.purchaseId, function (err, res){
+            for (var i = 0; i < res.length; i++) {
+
+                if (answer.input > res[i].stock) {
+
+                    console.log("================================");
+                    console.log("=====Insufficient quantity!=====");
+                    console.log("================================");
+                } else {
+                    console.log("================================");
+                    console.log("Thank you, we will send that right out");
+                    console.log("- - - - - - - - - - - - - - - - -");
+                    console.log("You have selected:");
+                    console.log("Item:" + res[i].product_name);
+                    console.log("Department:" + res[i].department_name);
+                    console.log("Quantity:" + res[i].answer.quantity);
+                    console.log("- - - - - - - - - - - - - - - - - ");
+                    console.log("Total:" + res[i].price * answer.quantity);
+                }
+                
+            }
+            //console.log(res);
+        });
+       
+        console.log("This is what you are ordering:" );   
+})
+
+//=======================updating database after purchase==========================
+
+.then(function(updateTable) {
+    connection.query("UPDATE products SET stock = stock - 'answer.quantity' WHERE id = 'answer.purchaseId' ", function (err, res) {
+console.log(res);
+    });
+});
+
 }
 
